ec02e441a593fe8cab58a44768932c19debf9525
andcarter95 <and.carter95@gmail.com>
2022-10-26 00:51:21 -0500

    Home screen design mostly done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,39 @@
 
 import "./styles/App.module.scss";
 
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Outlet } from "react-router-dom"
 
 //components
 import PageContainer from "./components/Containers/PageContainer";
 import Navbar from "./components/Navbar/Navbar"
 import MobileNavbar from "./components/Navbar/MobileNavbar";
+import MainContainer from "./components/Containers/MainContainer";
 
 //PAGES
 import Auth from "./pages/Auth";
+import Home from "./pages/Home";
 
 function App() {
   return <div className="App">
     <PageContainer optionClass={"pageContainer"}>
       <Navbar className="navbar"/>
-      
       <Routes>
-        <Route>
+          {/*AUTH*/}
           <Route path="/auth" element={<Auth/>}/>
+          {/*PROTECTED ROUTES*/}
+          <Route element={<Outlet/>}>
+          {/*HOME*/}
+          <Route path="/" element={<Home/>}/>
+
+            {/*404*/}
+            <Route 
+            path="/*"
+            element={
+              <MainContainer>
+                <span style={{fontSize:'1.2rem'}}>404 Page Not Found</span>
+              </MainContainer>
+            }
+            />
         </Route>
       </Routes>
       <div className="mobileMenu">
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,48 @@
+import MainContainer from "../components/Containers/MainContainer";
+import Searchbar from "../components/HomeContainer/Searchbar";
+import { Title } from "../components/Titles/Titles";
+import CategoryCard from "../components/Cards/CategoryCard";
+import TransactionCard from "../components/Cards/TransactionCard";
+
+import styles from "../styles/homeComponents/Home.module.scss"
+
+const Home = () => {
+
+
+    return (
+        <MainContainer optionClass={styles.container}>
+            <div className={styles.main}>
+                {/* SEARCHBAR */}
+                <div className={styles.searchbar}>
+                    <Searchbar/>
+                </div>
+
+                {/* CATEGORIES */}
+                <div className={styles.categories}>
+                    <Title>Categories Last 30 Days</Title>
+                    <div className={styles.content}>
+                        <CategoryCard/>
+                        <CategoryCard/>
+                        <CategoryCard/>
+                        <CategoryCard/>
+                    </div>
+                </div>
+
+                {/* TRANSACTIONS */}
+                <div className={styles.transactions}>
+                    <Title>Latest Transactions</Title>
+                    <div className={styles.content}>
+                        <TransactionCard/>
+                        <TransactionCard/>
+                        <TransactionCard/>
+                        <TransactionCard/>
+                        <TransactionCard/>
+                        <TransactionCard/>
+                    </div>
+                </div>
+            </div>
+        </MainContainer>
+    )
+}
+
+export default Home;
